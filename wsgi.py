aff7fc8379bda2f81aa0f76af24dfdc757902af6
alxstan22 <60945513+alxstan22@users.noreply.github.com>
2020-02-28 16:30:29 -0800

    changed more app
    changed more instances of app to application

--- wsgi.py
+++ wsgi.py
@@ -3,14 +3,14 @@ from flask import request
 
 application = Flask(__name__)   #App Instance created
 
-@app.route("/")         #When URL ends in '/' print "Hello, World!"
+@application.route("/")         #When URL ends in '/' print "Hello, World!"
 def greetings():        #Create 'greetings' method
     return "Hello, World!"
 #if __name__ == "__main__":        # on running python app.py
 #    app.run()                     # run flask App Instance
 
 
-@app.route("/other")         #When URL ends in '/' print "Hello, World!"
+@application.route("/other")         #When URL ends in '/' print "Hello, World!"
 def amessage():        #Create 'greetings' method
     return "This is a different page!"
 
